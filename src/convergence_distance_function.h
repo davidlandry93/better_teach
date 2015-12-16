17dfdb94fe2ae7e5afcca61e262e898db80968e9
David Landry <davidlandry93@gmail.com>
2015-12-16 15:53:10 -0500

    remove absolute path in code

--- src/convergence_distance_function.h
+++ src/convergence_distance_function.h
@@ -56,9 +56,9 @@ namespace TeachRepeat {
 
         Transform computedPositionOfReading = preTransform * icpResult;
 
-        reference.saveToDisk("/home/david/dat/Debug/ref", std::to_string(nCalls) + ".vtk");
+        reference.saveToDisk("Debug/ref", std::to_string(nCalls) + ".vtk");
         reading.transform(computedPositionOfReading.inverse());
-        reading.saveToDisk("/home/david/dat/Debug/result", std::to_string(nCalls++) + ".vtk");
+        reading.saveToDisk("Debug/result", std::to_string(nCalls++) + ".vtk");
         reading.transform(computedPositionOfReading);
 
         return (computedPositionOfReading.translationPart() - preciseReadingPosition.translationPart()).squaredNorm();
