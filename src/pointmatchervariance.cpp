2625a42fb966f35d07697aa8c1594b25d0eca332
David Landry <davidlandry93@gmail.com>
2016-01-19 09:42:36 -0500

    Now have the complete script untested

--- src/pointmatchervariance.cpp
+++ src/pointmatchervariance.cpp
@@ -9,6 +9,18 @@ namespace po = boost::program_options;
 
 static const int N_SAMLE_PER_PAIR = 100;
 
+float variance(const std::vector<float>& samples) {
+    float sum = std::accumulate(samples.begin(), samples.end(), 0.0);
+    float mean = sum / samples.size();
+
+    float acc = 0;
+    for(auto it = samples.begin(); it != samples.end(); it++) {
+        acc += (*it - mean) * (*it - mean);
+    }
+
+    return acc;
+}
+
 int main(int argc, char** argv) {
     po::options_description desc("Options");
 
@@ -30,13 +42,22 @@ int main(int argc, char** argv) {
     Map map(vm["map"].as< std::string >());
     std::cout << "Done." << std::endl;
 
-    PointMatcherService pmService;
+    PointMatcherService<float> pmService;
     pmService.loadConfigFile(vm["icpconfig"].as< std::string >());
 
+    std::vector<float> variances;
     for(auto it = map.begin(); it < it + 10; it += 2) {
+        std::vector<float> values;
         for(int i = 0; i < N_SAMLE_PER_PAIR; i++) {
+            Transform tFromReadingToReference = it->getPosition().transFromPose(map.begin()->getPosition());
+            Transform icpResult = pmService.icp(*it, *map.begin(), tFromReadingToReference);
 
+            float new_value = (icpResult.matrix().matrix() - tFromReadingToReference.matrix().matrix()).norm();
+            std::cout << new_value << std::endl;
+            values.push_back(new_value);
         }
+        variances.push_back(variance(values));
+        std::cout << "Variance: " << variance(values) << std::endl;
     }
 
     return 0;
