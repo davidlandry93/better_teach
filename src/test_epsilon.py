a9aa959a203f058f4a9051aee08baff5e03caad5
David Landry <davidlandry93@gmail.com>
2016-02-28 14:04:27 -0500

    Added a script for epsilons

--- src/test_epsilon.py
+++ src/test_epsilon.py
@@ -7,11 +7,16 @@ import subprocess
 
 DELTA = 0.25
 
+epsilons_rot = (0.09, 0.17)
+epsilons_trans = (0.05, 0.1, 0.15, 0.2, 0.25, 0.30)
+
 if __name__ == '__main__':
 
-    for rotationError in (0.13, 0.17, 0.26):
-        for lateralError in map(lambda x: x * DELTA, range(1,13)):
-            name_of_dir = time.strftime("%d-%m-%Y-" + str(lateralError) + str(lateralError))
+    for epsilon_rot in epsilons_rot:
+        for epsilon_trans in epsilons_trans:
+            name_of_dir = time.strftime("%d-%m-%Y-" +
+                                        str(epsilon_rot) + "_" +
+                                        str(epsilon_trans))
 
             try:
                 os.mkdir(name_of_dir)
@@ -21,16 +26,17 @@ if __name__ == '__main__':
             os.chdir(name_of_dir)
             logfile = open("log.txt", 'w')
 
-            print("Starting the run for " + str(lateralError) + "with rotation " + str(rotationError))
+            print("Starting the run for " + str(epsilon_trans) +
+                  "with rotation " + str(epsilon_rot))
             result = subprocess.call(["optimizeteachrepeatmain",
-                                      "-a", str(lateralError),
-                                      "-b", str(lateralError),
-                                      "-c", str(rotationError),
+                                      "-a", str(1.0),
+                                      "-b", str(1.0),
+                                      "-c", str(0.17),
                                       "-i", "../../IcpConfig.yaml",
-                                      "-e", str(0.20), "-r", str(0.09),
+                                      "-e", str(epsilon_trans),
+                                      "-r", str(epsilon_rot),
                                       "-m", "../teach/"],
                                      stdout=logfile)
-
 
             logfile.close()
             os.chdir("..")
